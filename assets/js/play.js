9df0ba87cf3cb9b3890f78f73663f104f61d85f9
Dan <dekay01@hotmail.com>
2020-04-24 20:43:38 -0400

    Feature:  adding playground link for js

---
+++ assets/js/play.js
@@ -0,0 +1,54 @@
+const myDataCollection = {
+    items: [
+        {
+            id: "1",
+            name: "name1"
+        },
+        {
+            id: "2",
+            name: "name2"
+        }
+    ]
+};
+
+
+class DisplayableProvider {
+    constructor() {
+        this.data = myDataCollection;
+    }
+
+    getDisplayables() {
+        const displayables = this.data.items.forEach(item => {
+            return new Displayable(item);
+        });
+        return displayables;
+    }
+}
+
+class NameAsTextDisplayer {
+    constructor(element) {
+        this.element = element;
+    }
+
+    display(displayable) {
+        this.element.innerText = displayable.content.name;
+    }
+}
+
+class Displayable {
+    constructor(content) {
+        this.content = content;
+    }
+}
+
+
+function main() {
+    const elementId = "dynamicdiv";
+    const displayInElement = document.getElementById(elementId);
+    const displayer = new NameAsTextDisplayer(displayInElement);
+    const displayableProvider = new DisplayableProvider();
+    const displayable = displayableProvider.getDisplayables();
+    displayer.display(displayable);
+}
+
+main();
