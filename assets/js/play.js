036585bab4d6ea5ccc826e5b976b029ec1c0d524
Dan <dekay01@hotmail.com>
2020-04-26 16:36:50 -0400

    Changing to use fetch api

--- assets/js/play.js
+++ assets/js/play.js
@@ -7,21 +7,25 @@ class ResourceCollection {
     }
 
     async fetch() {
-        const xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = () => {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                console.log('Fetched data.')
-                console.log(xmlhttp.responseText);
-                this.data = JSON.parse(xmlhttp.responseText);
-                console.log(this.data);
-            } else {
-                console.log('Error fetching data.')
-                console.log(xmlhttp);
-            }
-        }
-        xmlhttp.open("GET", this.resourceUrl, true);
-        xmlhttp.send();
+        const response = await this.fetch(this.resourceUrl);
+        this.data = await response.json();
     }
+    // async fetch() {
+    //     const xmlhttp = new XMLHttpRequest();
+    //     xmlhttp.onreadystatechange = () => {
+    //         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    //             console.log('Fetched data.')
+    //             console.log(xmlhttp.responseText);
+    //             this.data = JSON.parse(xmlhttp.responseText);
+    //             console.log(this.data);
+    //         } else {
+    //             console.log('Error fetching data.')
+    //             console.log(xmlhttp);
+    //         }
+    //     }
+    //     xmlhttp.open("GET", this.resourceUrl, true);
+    //     xmlhttp.send();
+    // }
 }
 
 function main() {
