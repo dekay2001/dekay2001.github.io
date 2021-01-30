3b210b918991d666e3520f62edde38e78650a459
Dan <dekay01@hotmail.com>
2021-01-29 19:59:28 -0500

    Fix: typo creating button on play

--- assets/js/yoga/play-sequence.js
+++ assets/js/yoga/play-sequence.js
@@ -157,7 +157,7 @@ class YogaSequenceCollection {
     nextDisplayable() {
         this.nextIndex++;
         if (this.nextIndex <= this.data.items.length) {
-            return new this._newYogaPose(this.nextIndex - 1);
+            return this._newYogaPose(this.nextIndex - 1);
         }
         return null;
     }
